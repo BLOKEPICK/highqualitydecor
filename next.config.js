ed150a10018a2ceade5b07a311081a0242268bb3
BLOKEPICK <Blokeplaycom@gmail.com>
2025-08-21 21:08:27 -0400

    UP

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-};
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Content-Security-Policy', value: `default-src 'self'; base-uri 'self'; img-src 'self' data: blob:; media-src 'self' data: blob:; connect-src 'self' https:; style-src 'self' 'unsafe-inline'; script-src 'self'; font-src 'self' data:; frame-ancestors 'self'; ` },
+        ],
+      },
+    ]
+  },
+}
 module.exports = nextConfig;
