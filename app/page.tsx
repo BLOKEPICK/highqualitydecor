6aa1366e9635c1130445d425cc6f6cfdb4f4628b
BLOKEPICK <Blokeplaycom@gmail.com>
2025-08-21 20:43:32 -0400

    UP

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-import Header from '@/components/Header'
-import BookForm from '@/components/BookForm'
-import Footer from '@/components/Footer'
+import Header from '../components/Header'
+import BookForm from '../components/BookForm'
+import Footer from '../components/Footer'
 
 export default function Home(){
   return (
