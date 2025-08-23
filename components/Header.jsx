37d3ae43764e6aaffd8cde5ccf41da5324157383
BLOKEPICK <Blokeplaycom@gmail.com>
2025-08-23 01:10:04 -0400

    up

--- components/Header.jsx
+++ components/Header.jsx
@@ -111,15 +111,18 @@ export default function Header() {
             <div className="social-row" aria-label="Redes sociales">
               <a href="#" aria-label="Instagram" target="_blank" rel="noopener noreferrer" className="social-link">
                 {/* Instagram glyph */}
-                <svg className="icon-ig" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fill="currentColor" d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zm0 2a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3H7zm5 3.5a5.5 5.5 0 1 1 0 11.001A5.5 5.5 0 0 1 12 7.5zm0 2a3.5 3.5 0 1 0 0 7.001 3.5 3.5 0 0 0 0-7.001zM18 6.5a1 1 0 1 1 .001 2.001A1 1 0 0 1 18 6.5z"/>
-                </svg>
+                <svg  className="icon-ig" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+                    <rect x="4" y="4" width="16" height="16" rx="4" stroke="currentColor" strokeWidth="2"/>
+                    <circle cx="12" cy="12" r="4.2" stroke="currentColor" strokeWidth="2"/>
+                    <circle cx="17" cy="7" r="1.2" fill="currentColor"/>
+                  </svg>
               </a>
               <a href="#" aria-label="Facebook" target="_blank" rel="noopener noreferrer" className="social-link">
                 {/* Facebook glyph */}
-                <svg className="icon-fb" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fill="currentColor" d="M13.5 9H16V6h-2.5C11.57 6 10 7.57 10 9.5V11H8v3h2v7h3v-7h2.1l.9-3H13v-1.5c0-.28.22-.5.5-.5z"/>
-                </svg>
+                <svg  className="icon-fb" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+                    <rect x="4" y="4" width="16" height="16" rx="4" stroke="currentColor" strokeWidth="2"/>
+                    <path d="M13.5 9H15V7h-1.5c-1.4 0-2.5 1.1-2.5 2.5V11H9v2h2v4h2v-4h1.7l.6-2H13v-1.5c0-.28.22-.5.5-.5z" fill="currentColor"/>
+                  </svg>
               </a>
             </div>
 
